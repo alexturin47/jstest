161afff79255c1d40724ae2bbf0c0a780cfac287
Alexander Tyurin <alexturin@gmail.com>
2018-06-29 15:16:05 +0700

    origin

--- js/script.js
+++ js/script.js
@@ -13,9 +13,17 @@ var mainList = {
 }
 mainList.budget = budget;
 mainList.shopName = shopName;
-for (var i = 0; i <= 2; i++) {
-	mainList.shopGoods[i] = prompt("Какой тип товаров будем продавать?", "");
-}
+for (var i = 0; i <= 5; i++) {
+	let a = prompt("Какой тип товаров будем продавать?");
+
+	if ((typeof(a)) === 'string' && a != '' && a.length < 50 ) {
+		console.log('Все верно');
+		mainList.shopGoods[i] = a;
+		} else if ((typeof(a)) != null) {
+				--i;
+			};
+		console.log(a);
+	}
 
 alert("Бюджет на 1 день = " + mainList.budget / 30 );
 
