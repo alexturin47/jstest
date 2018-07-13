82cf9cdc640906bbc43a9f36c2f6e1fb540c2fb3
Alexander Tyurin <alexturin@gmail.com>
2018-07-13 11:57:29 +0700

    пятый

--- js/script.js
+++ js/script.js
@@ -10,86 +10,89 @@ let budget,
 // 		budget = prompt("Ваш бюджет?");
 // 	}
 
-// 	shopName = prompt("Название вашего магазина").toUpperCase(); 
+// 	shopName = prompt("Название вашего магазина", "").toUpperCase(); 
 // 	time = 21;
 // }
 
 // start();
 
-var employer = {
+let employer = {
 	number: 1,
 	name: ""
 }
 
-var mainList = {
+let mainList = {
 	budget: 0,
 	shopName: "",
 	shopGoods: [],
 	employers: [],
 	open: true,
-	discount: false
-}
+	discount: false,
+	shopItems: [],
+	chooseGoods: function chooseGoods(){
+								for (var i = 0; i <= 5; i++) {
+									let a = prompt("Какой тип товаров будем продавать?");
 
-// mainList.budget = budget;
-// mainList.shopName = shopName;
-
-// function chooseGoods(){
-// 	for (var i = 0; i <= 5; i++) {
-// 		let a = prompt("Какой тип товаров будем продавать?");
-
-// 		if ((typeof(a)) === 'string' &&  ((typeof(a)) != null) &&  a != '' && a.length < 50 ) {
-// 			console.log('Все верно');
-// 			mainList.shopGoods[i] = a;
-// 			} else {
-// 					i = i -1;
-// 				};
-// 			console.log(a);
-// 		}
-// }
-
-// chooseGoods();
-
-// function workTime (time) {
-// 	if ( time < 0 ) {
-// 		console.log('Такого не может быть');
-// 		} else if ( time > 0 && time < 20){
-// 				console.log('Время работать');
-// 			} else if ( time < 24){
-// 					console.log('Уже слишком поздно');
-// 				} else {
-// 						console.log('В сутках только 24 часа');
-// 					} 
-// }
-
-// function calcDayBudget (budget) {
-// 	alert("Бюджет на 1 день = " + budget / 30 ); 
-// }
-
-// calcDayBudget(mainList.budget);
-
-// function setDiscount (disc) {
-// 	if (disc === true) {
-// 		price = price * 0.8;
-// 	} 
-// }
-
-function getEmployers () {
-	for( let i = 0; i < 4; i++) {
-		let empName = prompt('Имя сотрудника:');
-		while ( empName === "" || empName === null || (typeof(empName)) != 'string' ) {
-			empName = prompt('Имя сотрудника:');
-		}
-		employer = new Object();
-		employer.name = empName;
-		employer.number = i+1;
-		console.log(employer);
-		mainList.employers.push(employer);
-		console.log(mainList.employers);	
+									if ((typeof(a)) === 'string' &&  ((typeof(a)) != null) &&  a != '' && a.length < 50 ) {
+										console.log('Все верно');
+										mainList.shopGoods[i] = a;
+										} else {
+												i = i -1;
+											};
+										console.log(a);
+									}
+	},
+	workTime: function workTime (time) {
+							if ( time < 0 ) {
+								console.log('Такого не может быть');
+								} else if ( time > 0 && time < 20){
+										console.log('Время работать');
+										mainList.open = true;
+									} else if ( time < 24){
+											console.log('Уже слишком поздно');
+										} else {
+												console.log('В сутках только 24 часа');
+											} 
+	},
+	calcDayBudget: function calcDayBudget (budget) {
+										alert("Бюджет на 1 день = " + budget / 30 ); 
+	},
+	setDiscount: function setDiscount (disc) {
+									if (disc === true) {
+										price = price * 0.8;
+									} 
+	},
+	getEmployers: function getEmployers () {
+									for( let i = 0; i < 4; i++) {
+										let empName = prompt('Имя сотрудника:');
+										while ( empName === "" || empName === null || (typeof(empName)) != 'string' ) {
+											empName = prompt('Имя сотрудника:');
+										}
+										employer = new Object();
+										employer.name = empName;
+										employer.number = i+1;
+										console.log(employer);
+										mainList.employers.push(employer);
+										console.log(mainList.employers);	
+									}
+	},
+	chooseShopItem: function chooseShopItem () {		
+		let items = prompt("Перечислите через запятую ваши товары", "");
+		mainList.shopItems = items.split(",");
+		mainList.shopItems.push(prompt("Подождтите, еще ", ""));
+		mainList.shopItems.sort();
+	},
+	listShopItem: function listShopItem () {
+		mainList.shopItems.forEach(function(item,i,arr){
+			console.log(item + ' ' + i);
+		});
 	}
 }
 
-getEmployers();
 
-console.log(mainList.employers);
+mainList.chooseShopItem();
+mainList.listShopItem();
+
+console.log(mainList);
 
 
